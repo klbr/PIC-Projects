ad23df8e60df982d9ab2a7479d8b8cf23d0efa43
Kleber Bernardo <klbr.bernardo@gmail.com>
2012-03-08 00:27:16 -0300

    Release 1.0
    Bugs para futuros fixes - Displays Dez e Und piscam quando a centena é exibida.

--- ParkingSystem/ParkingSystem.c
+++ ParkingSystem/ParkingSystem.c
@@ -3,26 +3,25 @@
 #use delay (clock=1000000)
 #fuses XT,NOWDT,NOPUT
 /************************************************/ 
-long int Input = 0;
-long int Output = 0;
-long int Total = 0;
 
+//registra as quantidades que entraram e saíram
+long int Input = 99;
+long int Output = 0;
+
+//controlador de status da cancelas e sensores
 int ActiveIn = 0;
 int ActiveOut = 0;
 
-//int vtimer2 = 0;
-//long int secondsIn = 0;
-//long int secondsOut = 0;
-		
 void initState()
 {
 	//definicoes do PIC
     setup_timer_0(RTCC_INTERNAL | RTCC_DIV_16); 
-    //set_timer0(131);
-	enable_interrupts(GLOBAL | INT_TIMER0 | INT_RB);
-    set_timer0(131);	
+	enable_interrupts(GLOBAL | INT_TIMER0 | INT_RB | INT_EXT);
+    set_timer0(131);
+	//Zerando os displays
 	output_c(DEZENA[0] | UNIDADE[0]);
-	output_d(UNIDADE[0]);
+	output_d(DEZENA[0] | UNIDADE[0]);
+	//Mostra o total estacionado
 	dType = ALL;
 }
 
@@ -30,71 +29,99 @@ void initState()
 void trata_timer0() 
 { 
     set_timer0(131 + get_timer0()); 
-	//vtimer2++;
-	//if (vtimer2 == 125)
-	//{
-	//	secondsIn++;
-	//	secondsOut++;
-	//	vtimer2 = 0;
-	//}
+	//Tive que tratar o input_a e output_a aqui
+
+	//ativando o sensor entrada 1, caso não esteja lotado
+	if (input(INPUT_A) == 0)
+	{
+		if (MAX_VAGAS <= Input - Output)
+			return;
+		if (ActiveIn != 2)
+			ActiveIn = 1;
+	}
+	
+	//ativando o sensor de saida 1, caso exista carros estacionados
+	//senao ficaria negativo
+	if (input(OUTPUT_A) == 0)
+	{
+		if (Input - Output <= 0)
+			return;
+		if (ActiveOut != 2)
+			ActiveOut = 1;
+	}
 } 
+
+#int_ext
+void trata_ext()
+{
+	//Motorista recebe o ticket e a cancela se abre,
+	if (input(GET_TICKET) == 0)
+	{
+		// caso esteja lotado, sai, não abre a cancela e nem da o ticket
+		if (MAX_VAGAS <= Input - Output)
+			return;
+		if (ActiveIn == 1)
+			InOpen();
+	}
+}
 
 #int_rb
 void trata_rb()
 {
 	int x;
-	//representa o Laço Indutivo 01
-	//zera os contadores para iniciar uma nova contagem de tempo 
+
+	//muda a exbibição dos displays
 	if (input(CHANGE) == 0)
 	{		
 		switch(dType)
 		{
 			case IN_C:
+				//Apenas as saidas
 				dType = OUT_C;
 				break;
 			case OUT_C:
+				//total estacionado
 				dType = ALL;
 				break;
 			case ALL:
-				dType = Input;
+				//total que entrou
+				dType = IN_C;
 				break;
 		}
 	}
-
-	if (input(IN1) == 0)
-	{	
-		if (ActiveIn == 0)
-			InOpen();
-	}
-	if (input(IN2) == 0)
+	
+	//fecha a cancela depois q o motorista passa pelo sensor 2
+	if (input(INPUT_CAN) == 0)
 	{		
-		if (ActiveIn == 1)
+		if (ActiveIn == 2)
 		{
 			InClose();
-			Total++;
 		}		
 	}
 	
-	if (input(OUT1) == 0)
-	{	
-		if (ActiveOut == 0)		
+	//ao colocar o ticket a cancela abre
+	if (input(PUT_TICKET) == 0)
+	{
+		if (Input - Output <= 0)
+			return;
+		if (ActiveOut == 1)
 			OutOpen();
 	}
-	if (input(OUT2) == 0)
+	//depois que o carro sai, a cancela fecha
+	if (input(OUTPUT_CAN) == 0)
 	{		
-		if (ActiveOut == 1)
+		if (ActiveOut == 2)
 			OutClose();	
 	}
-	
 	x = input_b();
 }
 
 void ShowValueOnDisplay()
 {
 	long int local = 0;
-	int cen = 0;
-	int dec = 0;
-	int unt = 0;	
+	long int cen = 0;
+	long int dec = 0;
+	long int unt = 0;	
 	switch(dType)
 	{
 		case IN_C:
@@ -104,7 +131,7 @@ void ShowValueOnDisplay()
 			local = Output;
 			break;
 		case ALL:
-			local = Total;
+			local = Input - Output;
 			break;
 	}
 	if (local >= 999)
@@ -118,45 +145,50 @@ void ShowValueOnDisplay()
 		cen = local / 100;
 		dec = local / 10;
 		//simula o MOD
-		unt = local - (dec * 10);		
+		unt = local - (dec * 10) - (cen * 100); 
 	}
 	output_c(DEZENA[dec] | UNIDADE[unt]);
-	output_d(UNIDADE[cen]);
+	output_d(DEZENA[0] | UNIDADE[cen]);
+}
+
+void InOpen()
+{
+	output_high(CANCELA_IN);
+	ActiveIn = 2;	
+}
+//ao fechar a cancela da entrar o valor é incrementado
+void InClose()
+{
+	output_low(CANCELA_IN);
+	ActiveIn = 0;
+	Input++;
+	//lotado é ativado depois q o carro entra
+	//e se esse carro for o máximo permitido
+	if (MAX_VAGAS <= Input - Output)	
+		output_high(LOTADO);
+}
+
+void OutOpen()
+{
+	output_high(CANCELA_OUT);
+	ActiveOut = 2;	
+}
+//ao fechar a cancela de saida o valor é incrementado
+void OutClose()
+{
+	//ao sair pelo menos um carro, o lotado deve apagar
+	output_low(CANCELA_OUT);
+	output_low(LOTADO);
+	ActiveOut = 0;
+	Output++;
 }
 
 void start()
 {
 	while(TRUE)
 	{
-		//Mostrando os valores nos displays
 		ShowValueOnDisplay();
 	}
-}
-
-void InOpen()
-{
-	output_high(CANCELA_IN);
-	ActiveIn = 1;	
-	Input++;
-//	secondsIn = 0; 
-}
-void InClose()
-{
-	output_high(CANCELA_IN);
-	ActiveIn = 0;
-}
-
-void OutOpen()
-{
-	output_high(CANCELA_OUT);
-	ActiveOut = 1;
-	Output++;
-//	secondsOut = 0;
-}
-void OutClose()
-{
-	output_high(CANCELA_OUT);
-	ActiveOut = 0;
 }
 
 void main()
--- ParkingSystem/ParkingSystem.h
+++ ParkingSystem/ParkingSystem.h
@@ -1,12 +1,28 @@
-#define IN1 PIN_B4
-#define IN2 PIN_B5
-#define OUT1 PIN_B6
-#define OUT2 PIN_B7
+//Portas dos sensores e do ticket da cancela de entrada
+#define INPUT_A PIN_B1
+#define INPUT_CAN PIN_B4
+#define GET_TICKET PIN_B3
 
-#define CHANGE PIN_A2
-#define CANCELA_IN PIN_A3
-#define CANCELA_OUT PIN_A4
+//Portas dos sensores e do ticket da cancela de saída
+#define OUTPUT_A PIN_B2
+#define OUTPUT_CAN PIN_B7
+#define PUT_TICKET PIN_B6
 
+//Porta para a mudança dos Valores dos display das quantidades
+//Entrada - Saida - Estacionado
+#define CHANGE PIN_B5
+
+//Leds que indicam a subida da cancela 
+#define CANCELA_IN PIN_A0
+#define CANCELA_OUT PIN_A1
+
+//Led que indica que está lotado o estacionamento
+#define LOTADO PIN_A2
+
+//Quantidade máxima de vagas
+long int MAX_VAGAS = 100;
+
+//Estados dos displays que é alterado pelo click do CHANGE 
 enum DisplayType { IN_C, OUT_C, ALL };
 
 DisplayType dType;
@@ -41,11 +57,11 @@ const byte UNIDADE[10] =
 
 
 /*
-  Exemplo: 12 == CENTENA[0] | DEZENA[1] | UNIDADE[2];
+  Exemplo: 112 == CENTENA[1] | DEZENA[1] | UNIDADE[2];
 */
 
 /*
-  Inicia os valores da lombada eletronica e inicia o timer/int_b;
+  Inicia os valores do sistema e inicia o timer/int_b/int_ext;
 */
 void initState();
 
@@ -60,10 +76,10 @@ int SetTicket();
 */
 void InOpen();
 void InClose();
-
 void OutOpen();
 void OutClose();
+
 /*
-  Mostra no display a velocidade em Km/h
+  Mostra no display a quantidade selecionada dos carros
 */
 void ShowValueOnDisplay();
